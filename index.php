1bd64dab3ce060385b4c829b9469a4e29870021b
nguyenthonghuy16012003 <huyten26@gmail.com>
2024-07-11 00:59:06 +0700

    Clone giao dien

--- index.php
+++ index.php
@@ -1,2 +1,13 @@
 <?php   
-    session_start();
+    // Đây là Model
+require_once "model/taikhoan.php";
+require_once "model/product.php";
+require_once "model/category.php";
+
+
+    // Đây là Controller
+require_once "controller/main.php";
+require_once "controller/login.php";
+require_once "controller/product_controller.php";
+
+?>
